a20c48d86b73b29867c89e980ccd38d5d86ec143
Anand Singh <anand.s@marmeto.com>
2024-06-22 12:43:51 +0530

    Add marmeto-shipping-bar-v2

---
+++ Readme.txt
@@ -0,0 +1,86 @@
+Paste the below code in theme.liquid just above the body tag:
+
+<script>
+  let priceval1 = {{settings.first_shipping_threshold }}
+  let priceval2 = {{ settings.second_shipping_threshold }}
+  let priceval3 ={{ settings.third_shipping_threshold }}
+
+  let dis_coupon_msg1 = "{{ settings.first_threshold_message }}"
+  let dis_coupon_msg2 = "{{ settings.second_threshold_message }}"
+  let dis_coupon_msg3 = "{{ settings.third_threshold_message }}"
+
+  let finalThresholdMessage = "{{ settings.final_threshold_message }}"
+</script>
+
+This code creates variables with the values received from global settings and can be used anywhere in the code base.
+
+
+Paste the below code in settings_schema.json:
+
+
+{
+    "name": "Free shipping bar",
+    "settings": [
+      {
+        "type": "richtext",
+        "id": "free_shipping_heading",
+        "label": "Heading",
+        "default":"<p>Free shipping</p>"
+      },
+      {
+        "type": "text",
+        "id": "first_shipping_threshold",
+        "label": "First shipping threshold"
+      },
+      {
+        "type": "text",
+        "id": "first_threshold_message",
+        "label": "First threshold message"
+      },
+      {
+        "type": "image_picker",
+        "id": "first_threshold_image",
+        "label": "First threshold image"
+      },
+      {
+        "type": "text",
+        "id": "second_shipping_threshold",
+        "label": "Second shipping threshold"
+      },
+      {
+        "type": "text",
+        "id": "second_threshold_message",
+        "label": "Second threshold message"
+      },
+      {
+        "type": "image_picker",
+        "id": "second_threshold_image",
+        "label": "Second threshold image"
+      },
+      {
+        "type": "text",
+        "id": "third_shipping_threshold",
+        "label": "Third shipping threshold"
+      },
+      {
+        "type": "text",
+        "id": "third_threshold_message",
+        "label": "Third threshold message"
+      },
+      {
+        "type": "image_picker",
+        "id": "third_threshold_image",
+        "label": "Third threshold image"
+      },
+      {
+        "type": "text",
+        "id": "final_threshold_message",
+        "label": "Final threshold message"
+      },
+      {
+        "type": "text",
+        "id": "threshold_price_limit",
+        "label": "Threshold price limit"
+      }
+    ]
+  },
---
+++ assets/shipping-bar.js
@@ -0,0 +1,100 @@
+class MarmetoShippingBarV2 extends HTMLElement {
+  constructor() {
+    super();
+    this.initShippingBar();
+  }
+
+  initShippingBar() {
+    document.addEventListener("click", function (event) {
+      if (
+        event.target.classList.contains("cart-icon") ||
+        event.target.classList.contains("quantity__button")
+      ) {
+        setTimeout(function () {
+          fetch("/cart.js")
+            .then((response) => response.json())
+            .then((cart) => {
+              this.total_price = cart.total_price;
+              this.cart_total = total_price / 100;
+              this.subtotal = cart_total;
+
+              document
+                .querySelectorAll(".shipping-threshold")
+                .forEach((item) => item.classList.remove("active"));
+              this.discountProgress = document.querySelector(
+                ".shipping-bar__indicator"
+              );
+              this.discountProgress.classList.remove(
+                "step1",
+                "step2",
+                "step3",
+                "step4"
+              );
+
+              if (subtotal < priceval1) {
+                let away_amt = priceval1 - subtotal;
+                document.querySelector(".discount-away-amt").innerHTML = "";
+                document.querySelector(".discount-away-amt").innerHTML =
+                  "₹" + away_amt;
+                document.querySelector(".discount-away-next-offer").innerHTML =
+                  "";
+                document.querySelector(".discount-away-next-offer").innerHTML =
+                  dis_coupon_msg1;
+              } else if (subtotal > priceval1 && subtotal < priceval2) {
+                let away_amt1 = priceval2 - subtotal;
+                document.querySelector(".discount-away-amt").innerHTML = "";
+                document.querySelector(".discount-away-amt").innerHTML =
+                  "₹" + away_amt1;
+                document.querySelector(".discount-away-next-offer").innerHTML =
+                  "";
+                document.querySelector(".discount-away-next-offer").innerHTML =
+                  dis_coupon_msg2;
+
+                this.discountProgress.className =
+                  "shipping-bar__indicator step1";
+                document
+                  .querySelectorAll(".threshold-image")[0]
+                  ?.classList.add("active");
+              } else if (subtotal > priceval2 && subtotal < priceval3) {
+                let away_amt2 = priceval3 - subtotal;
+                console.log(away_amt2);
+                document.querySelector(".discount-away-amt").innerHTML = "";
+                document.querySelector(".discount-away-amt").innerHTML =
+                  "₹" + away_amt2;
+                document.querySelector(".discount-away-next-offer").innerHTML =
+                  "";
+                document.querySelector(".discount-away-next-offer").innerHTML =
+                  dis_coupon_msg3;
+
+                this.discountProgress.className =
+                  "shipping-bar__indicator step2";
+
+                document
+                  .querySelectorAll(".threshold-image")[0]
+                  ?.classList.add("active");
+                document
+                  .querySelectorAll(".threshold-image")[1]
+                  ?.classList.add("active");
+              } else {
+                this.discountProgress.className =
+                  "shipping-bar__indicator step4";
+                document
+                  .querySelectorAll(".threshold-image")[0]
+                  ?.classList.add("active");
+                document
+                  .querySelectorAll(".threshold-image")[1]
+                  ?.classList.add("active");
+                document
+                  .querySelectorAll(".threshold-image")[2]
+                  ?.classList.add("active");
+
+                  document.querySelector('.shipping-bar__message').innerHTML = `<p>${finalThresholdMessage}<p>`
+              }
+            });
+        }, 1000);
+      }
+    });
+  }
+}
+
+customElements.define('marmeto-shipping-bar-v2', MarmetoShippingBarV2)
